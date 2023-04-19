8877a0ab59925bb62da91469bff51a0b959274bb
neptune890 <davidlcasey@gmail.com>
2023-04-19 03:38:44 -0400

    Added onchatmessage listender and created chatlogs.txt

--- src/main/java/net/bitstobytes/runescape/plugintest/PluginTestConfig.java
+++ src/main/java/net/bitstobytes/runescape/plugintest/PluginTestConfig.java
@@ -8,11 +8,19 @@ import net.runelite.client.config.ConfigItem;
 public interface PluginTestConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+			keyName = "greeting",
+			name = "Welcome Greeting",
+			description = "The message to show to the user when they login"
 	)
 	default String greeting()
+	{
+		return "Hello";
+	}
+	@ConfigItem(
+			keyName = "greetingtwo",
+			name = "GreetingTwo",
+			description = "ATestConfigItem")
+	default String greetingtwo()
 	{
 		return "Hello";
 	}
--- src/main/java/net/bitstobytes/runescape/plugintest/PluginTestPlugin.java
+++ src/main/java/net/bitstobytes/runescape/plugintest/PluginTestPlugin.java
@@ -6,16 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.OverheadTextChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import static net.runelite.client.RuneLite.RUNELITE_DIR;
+//import net.runelite.http.api.RuneLiteAPI;
 
 @Slf4j
 @PluginDescriptor(
-	name = "PluginTest"
+		name = "PluginTest"
 )
 public class PluginTestPlugin extends Plugin
 {
@@ -25,16 +33,19 @@ public class PluginTestPlugin extends Plugin
 	@Inject
 	private PluginTestConfig config;
 
+	private static final File LOG_DIR = new File(RUNELITE_DIR, "Chatlogs");
+
+	private FileWriter myWriter;
+
 	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
+	protected void startUp() throws Exception {
+		log.info("Chatlogs started!");
+		LOG_DIR.mkdir();
 	}
 
 	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
+	protected void shutDown() throws Exception {
+		log.info("Chatlogs stopped!");
 	}
 
 	//Add event listeners using @Subscribe followed by the event
@@ -47,17 +58,32 @@ public class PluginTestPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+	public void onGameStateChanged(GameStateChanged gameStateChanged) {
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
+			//client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
 		}
 	}
 
+	//Add listener for when there is a chat message..
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage) {
+		ChatMessageType chatMessageType = chatMessage.getType();
+		// Action to take when receiving new chat message
+		// messageQueue.offer(chatMessage.getMessageNode());
+		try {
+			myWriter = new FileWriter(new File(LOG_DIR, "Chatlogs.txt"), true);
+			myWriter.write(chatMessageType.getType() + chatMessage.getTimestamp() + ":" + chatMessage.getName() + ":" + chatMessage.getMessage() + "\r\n");
+			myWriter.close();
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+	}
+
+
 	@Provides
-	PluginTestConfig provideConfig(ConfigManager configManager)
-	{
+	PluginTestConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(PluginTestConfig.class);
 	}
 }
